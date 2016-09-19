e2eacfac5160b230de19a503c78ba8ca177f79ba
吴朋朋 <wpwp1992@qq.com>
2016-09-19 10:25:07 +0800

    car

--- car.h
+++ car.h
@@ -4,6 +4,8 @@
 #include <rapidjson/document.h>
 #include <rapidjson/writer.h>
 #include <rapidjson/stringbuffer.h>
+#include <iostream>
+#include <time.h>
 using std::string;
 using std::vector;
 
@@ -27,9 +29,14 @@ public :
 class CarGenerator
 {
 public :
+	CarGenerator()
+	{
+		srand(time(NULL));
+	}
 	vector<Cross> crosses;
 	void GenerateCars(rapidjson::Value& carArray, rapidjson::Document::AllocatorType& allocator)
 	{
+		printf("cross size %d \n", crosses.size());
 		for (int i = 0; i < crosses.size(); ++i)//crosses(random)
 		{
 			rapidjson::Value car(rapidjson::kObjectType);
@@ -41,16 +48,37 @@ public :
 			position.AddMember("z", z, allocator);
 			car.AddMember("position", position, allocator);
 			//direction (random)
-			rapidjson::Value direction(crosses[i].orientation);
-			car.AddMember("direction", direction, allocator);
+			{
+				int randDir, randSize;
+				if ("tcross" == crosses[i].type || "tcross_greenlight" == crosses[i].type)
+					randSize = 3;
+				else if ("lcross" == crosses[i].type || "lcross_greenlight" == crosses[i].type)
+					randSize = 2;
+				else if ("xcross" == crosses[i].type || "xcross_greenlight" == crosses[i].type)
+					randSize = 4;
+
+				randDir = randSize * rand() * 1.0f / RAND_MAX;
+				if (randDir == randSize) randDir--;
+				int dir = (crosses[i].orientation + randDir) % 4;
+				if (!dir) dir = 4;
+
+				rapidjson::Value direction(dir);
+				car.AddMember("direction", direction, allocator);
+			}
 			//crossType
-			car.AddMember("crossType", crosses[i].type, allocator);
+			rapidjson::Value s;
+			s.SetString(rapidjson::StringRef(crosses[i].type.c_str()));
+			car.AddMember("crossType", s, allocator);
 			//car mesh (random)
-			rapidjson::Value meshID(1);
+			int carMeshId = 11 * rand() * 1.0f / RAND_MAX;
+			if (carMeshId == 11) carMeshId--;
+			rapidjson::Value meshID(carMeshId);
 			car.AddMember("mesh", meshID, allocator);
 			//car speed
-			rapidjson::Value speed(20.0f);
+			rapidjson::Value speed(1.0f);
 			car.AddMember("speed", speed, allocator);
+
+			carArray.PushBack(car, allocator);
 		}
 	}
 };
